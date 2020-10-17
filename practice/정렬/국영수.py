7a90fc75538e8bad492a56eb157decd41f5cce56
woorud <59363022+woorud@users.noreply.github.com>
2020-10-18 02:02:06 +0900

    Day 215

---
+++ practice/정렬/국영수.py
@@ -0,0 +1,9 @@
+n = int(input())
+students = []
+for i in range(n):
+    students.append(input().split())
+
+students.sort(key = lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for students in students:
+    print(students[0])
