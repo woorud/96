ad2430d97e3438e940e80f27937d8f700cf98b88
woorud <59363022+woorud@users.noreply.github.com>
2020-11-27 19:02:45 +0900

    Update 파일명 정렬.py

--- practice/Kakao/파일명 정렬.py
+++ practice/Kakao/파일명 정렬.py
@@ -4,8 +4,13 @@ def solution(files):
     for i in files:
         tmp = re.split("([0-9]+)", i)
         files_split.append(tmp)
-    return files_split
-
+    
+    files_sorted = sorted(files_split, key = lambda x : (x[0].upper(), int(x[1])))
+    
+    res = []
+    for i in files_sorted:
+        res.append("".join(i))
+    return res
 
 
 
