b802d128c9781d6f081abd83cdd730d49dac8528
woorud <59363022+woorud@users.noreply.github.com>
2021-01-04 21:00:27 +0900

    Day 281

--- practice/DFS와 BFS/숨바꼭질.py
+++ practice/DFS와 BFS/숨바꼭질.py
@@ -1,25 +1,19 @@
 from collections import deque
-n, k = map(int, input().split())
-visited = [0 for i in range(100001)]
+def _1697():
+    n, k = map(int, input().split())
+    array = [0 for _ in range(100001)]
+    def bfs(v):
+        q = deque()
+        q.append(v)
+        while q:
+            x = q.popleft()
+            if x == k:
+                print(array[x])
+                return
+            for i in (x-1, x+1, 2*x):
+                if 0 <= i < len(array) and not array[i]:
+                    array[i] = array[x] + 1
+                    q.append(i)
+    bfs(n)
 
-def bfs(v):
-    q = deque()
-    cnt = 0
-    q.append((v, cnt))
-
-    while q:
-        v = q.popleft()
-        a = v[0]
-        cnt = v[1]
-        if visited[a] == 0 and a == k:
-            return cnt
-        cnt += 1
-        if visited[a] == 0 and a*2 <= 100000:
-            q.append((a*2, cnt))
-        if visited[a] == 0 and a+1 <= 100000:
-            q.append((a+1, cnt))
-        if visited[a] == 0 and a-1 >= 0:
-            q.append((a-1, cnt))
-    return cnt
-
-print(bfs(n))
+_1697()
