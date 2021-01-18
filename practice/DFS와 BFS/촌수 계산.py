8352a1aeed77fba88687d92d2374a3d5ffc361e3
woorud <59363022+woorud@users.noreply.github.com>
2021-01-18 22:51:28 +0900

    Day 290

--- practice/DFS와 BFS/촌수 계산.py
+++ practice/DFS와 BFS/촌수 계산.py
@@ -1,31 +1,33 @@
-from collections import deque
+from collections import deque, defaultdict
+def _2644():
+    def bfs(v):
+        q = deque()
+        q.append(v)
+        visited[v] = True
+        cnt = 0
+        while q:
+            cnt += 1
+            for i in range(len(q)):
+                k = q.popleft()
+                if k == b:
+                    return cnt-1
 
-def bfs(v):
-    q = deque()
-    q.append(v)
-    visited[v] = True
-    ans = 0
-    while q:
-        ans += 1
-        for i in range(len(q)):
-            k = q.popleft()
-            if k == b:
-                return ans-1
-            for j in tree[k]:
-                if visited[j] == False:
-                    visited[j] = True
-                    q.append(j)
+                for j in family[k]:
+                    if visited[j] == False:
+                        q.append(j)
+                        visited[j] = True
+        return -1
 
-    return -1
+    n = int(input())
+    a, b = map(int, input().split())
+    m = int(input())
+    visited = [False]*(n+1)
+    family = defaultdict(lambda : [])
+    for _ in range(m):
+        x, y = map(int, input().split())
+        family[x].append(y)
+        family[y].append(x)
 
-n = int(input())
-a, b = map(int ,input().split(' '))
-m = int(input())
-tree = [[] for _ in range(n+1)]
-for _ in range(m):
-    x, y = map(int, input().split(' '))
-    tree[x].append(y)
-    tree[y].append(x)
+    print(bfs(a))
 
-visited = [False] * (n+1)
-print(bfs(a))
+_2644()
