f812bf1f9c30c37d262538596037929fff8a642a
woorud <59363022+woorud@users.noreply.github.com>
2021-01-21 16:52:27 +0900

    Day 291

--- practice/DFS와 BFS/알파벳.py
+++ practice/DFS와 BFS/알파벳.py
@@ -1,27 +1,28 @@
 import sys
-def BFS(x, y):
-    global answer
+def _1987():
+    global max_d
+    def bfs(i, j):
+        global max_d
 
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
 
-    q = set([(x, y, alpha[x][y])])
+        q = set([(i, j, arr[i][j])])
 
-    while q:
-        x, y, ans = q.pop()
+        while q:
+            x, y, alpha = q.pop()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < r and 0 <= ny < c and arr[nx][ny] not in alpha:
+                    q.add((nx, ny, alpha+arr[nx][ny]))
+                    max_d = max(max_d, len(alpha)+1)
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    r, c = map(int, sys.stdin.readline().split())
+    arr = [list(sys.stdin.readline()) for _ in range(r)]
+    max_d = 1
+    bfs(0, 0)
+    print(max_d)
 
-            if ((0 <= nx < R) and (0 <= ny < C)) and (alpha[nx][ny] not in ans):
-                q.add((nx, ny, ans + alpha[nx][ny]))
-                answer = max(answer, len(ans)+1)
+_1987()
 
-
-R, C = map(int, sys.stdin.readline().split())
-alpha = [list(sys.stdin.readline().strip()) for _ in range(R)]
-
-answer = 1
-BFS(0, 0)
-print(answer)
